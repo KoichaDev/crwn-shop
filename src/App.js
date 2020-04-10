bdb25ee5343c4555adab188fed151b63d153b2ed
Khoi Hoang <hello@koicha.dev>
2020-04-10 19:08:14 +0200

    add: :sparkles: Updating our app with a shop page, collection preview component and collection item-component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
-
-const HatsPage = () => (
-  <>
-    <h1>HATS PAGE</h1>
-  </>
-);
+import ShopPage from './Pages/Shop-page/shop.component';
 
 function App() {
   return (
@@ -15,7 +10,7 @@ function App() {
       <Switch>
         {/* exact is good for only rendering the specifc URL. It doesn't render multiple components in one page */}
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
