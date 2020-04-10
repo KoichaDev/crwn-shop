a058cffff4a4b19b0c466ef2f2ec5113672dad20
Khoi Hoang <hello@koicha.dev>
2020-04-10 19:21:53 +0200

    add: :sparkles: Adding header component and links to home, the shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/Shop-page/shop.component';
+import Header from './Components/header/header.component';
 
 function App() {
   return (
-    <div className='App'>
+    <>
+      <Header />
       <Switch>
         {/* exact is good for only rendering the specifc URL. It doesn't render multiple components in one page */}
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
-    </div>
+    </>
   );
 }
 
