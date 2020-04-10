25a5d2e8b4db59eac4849010ec7694dddacf2790
Khoi Hoang <hello@koicha.dev>
2020-04-10 18:29:19 +0200

    update: art our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-
 import HomePage from './Pages/Homepage/homepage.component';
+
+const HatsPage = () => (
+  <>
+    <h1>HATS PAGE</h1>
+  </>
+);
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      <Switch>
+        {/* exact is good for only rendering the specifc URL. It doesn't render multiple components in one page */}
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/Components/menu-item/menu-item.component.jsx
+++ src/Components/menu-item/menu-item.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+/* withRouter = HOC  */
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageURL, size }) => (
-  <div className={`${size} : menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className='background-image'
-      style={{ backgroundImage: `url(${imageURL})` }}
+      style={{ backgroundImage: `url(${imageUrl})` }}
     />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
@@ -14,4 +19,4 @@ const MenuItem = ({ title, imageURL, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
